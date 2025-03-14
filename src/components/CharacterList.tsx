2127cd23c6bcfeb724f61f4929a24232f9c1cbfc
RaZz <amirrahmati471@gmail.com>
2025-03-14 12:46:22 +0330

    add character components and navbar

---
+++ src/components/CharacterList.tsx
@@ -0,0 +1,36 @@
+import { EyeIcon } from "@heroicons/react/24/outline";
+import type { Character } from "../types/Character";
+
+function CharacterList({ characters }: { characters: Character[] }) {
+  return (
+    <div className="characters-list">
+      {characters.map((item) => (
+        <Character key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default CharacterList;
+
+function Character({ item }: { item: Character }) {
+  return (
+    <div className="list__item">
+      <img src={item.image} alt={item.name} />
+      <h3 className="name">
+        <span>{item.gender === "Male" ? "👨🏻" : "👩🏻"}</span>
+        <span>{item.name}</span>
+      </h3>
+      <div className="list-item__info">
+        <span
+          className={`status ${item.status === "Dead" ? "red" : ""}`}
+        ></span>
+        <span> {item.status} </span>
+        <span> - {item.species} </span>
+      </div>
+      <button className="icon red">
+        <EyeIcon/>
+      </button>
+    </div>
+  );
+}
